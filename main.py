e5a92fc85e7a932d72f6d2d17d97fbf8a16a60cd
zahlliu <zahlliu@tencent.com>
2023-06-09 17:56:33 +0800

    feat: add socks proxy support

--- main.py
+++ main.py
@@ -1,30 +1,52 @@
 import asyncio
 import os
 import sys
+import time
+import aiosocks
+
 
 import asyncssh
 
 
-async def run_scp(source_dir, target_dir, host, port, username, password=None, private_key=None) -> None:
-    client_keys = [asyncssh.import_private_key(private_key)] if private_key else []
-    async with asyncssh.connect(
-            host,
-            port=int(port),
-            username=username,
-            password=password,
-            known_hosts=None,
-            client_keys=client_keys
-    ) as conn:
-        print(f'{host}: Starting remove {target_dir}')
-        await conn.run(f'mkdir -p {target_dir}')
-        await conn.run(f'rm -rf {target_dir}')
-        await conn.run(f'mkdir -p {target_dir}')
-        print(f'{host}: Starting upload {source_dir} to {target_dir}')
-        await asyncssh.scp(srcpaths=source_dir, dstpath=(conn, f'{target_dir}'), recurse=True, preserve=True)
-        print(f'{host}: ok!')
+class SocksClientConnection:
+    def __init__(self, proxy):
+        user_pass, host_port = proxy.split('@')
+        self.username, self.password = user_pass.split(':')
+        self.host, self.port = host_port.split(':')
+
+    async def create_connection(self, session_factory, host, port):
+        socks5_addr = aiosocks.Socks5Addr(self.host, int(self.port))
+        socks5_auth = aiosocks.Socks5Auth(self.username, self.password)
+        return await aiosocks.create_connection(
+            session_factory, proxy=socks5_addr, proxy_auth=socks5_auth, dst=(host, port)
+        )
+
+async def run_scp(source_dir, target_dir, host, port, username, password=None, private_key=None, proxy=None) -> None:
+    try:
+        client_keys = [asyncssh.import_private_key(private_key)] if private_key else []
+        time_start = time.monotonic()
+        tunnel = SocksClientConnection(proxy) if proxy else ()
+        async with asyncssh.connect(
+                host,
+                port=int(port),
+                tunnel=tunnel,
+                username=username,
+                password=password,
+                known_hosts=None,
+                client_keys=client_keys
+        ) as conn:
+            print(f'{host}: Starting remove {target_dir}')
+            await conn.run(f'mkdir -p {target_dir}')
+            await conn.run(f'rm -rf {target_dir}')
+            await conn.run(f'mkdir -p {target_dir}')
+            print(f'{host}: Starting upload {source_dir} to {target_dir}')
+            await asyncssh.scp(srcpaths=source_dir, dstpath=(conn, f'{target_dir}'), recurse=True, preserve=True)
+            print(f'{host}: ok! cost: {time.monotonic() - time_start:.2f} s')
+    except Exception as e:
+        print(f'{host}: ERROR! {e} {type(e)}')
 
 
-async def run(source_dir, target_dir, host_raw, username, password=None, private_key=None):
+async def run(source_dir, target_dir, host_raw, username, password=None, private_key=None, proxy=None):
     tasks = []
     if not any((password, private_key)):
         raise ValueError('password and private_key must have one')
@@ -38,7 +60,7 @@ async def run(source_dir, target_dir, host_raw, username, password=None, private
         print(f'Scping: {host}:{port}')
         # await run_scp(source_dir, target_dir, host, port, username, password, private_key)
         # print(f'scp success: {host}:{port}')
-        tasks.append(run_scp(source_dir, target_dir, host, port, username, password, private_key))
+        tasks.append(run_scp(source_dir, target_dir, host, port, username, password, private_key, proxy))
     await asyncio.gather(*tasks, return_exceptions=True)
 
 
@@ -50,6 +72,7 @@ if __name__ == '__main__':
         username = os.environ.get('INPUT_USERNAME')
         password = os.environ.get('INPUT_PASSWORD')
         private_key = os.environ.get('INPUT_KEY')
-        asyncio.run(run(source_dir, target_dir, host_raw, username, password, private_key))
+        proxy = os.environ.get('INPUT_PROXY')
+        asyncio.run(run(source_dir, target_dir, host_raw, username, password, private_key, proxy))
     except (OSError, asyncssh.Error) as exc:
         sys.exit(f'SSH connection failed: ' + str(exc))
